764123f12125a4aabd58c6d012522dce1104ee23
Hazem Elsayed <hazemm.els@gmail.com>
2022-10-17 09:00:46 +0300

    added ListingScrape item and fields

--- UpHostScraper/UpHostScraper/items.py
+++ UpHostScraper/UpHostScraper/items.py
@@ -6,7 +6,29 @@
 import scrapy
 
 
-class UphostscraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class ListingScrape(scrapy.Item):
+    """Results of scraping a listing at a timestamp"""
+    # Numeric Types
+    timestamp = scrapy.Field()
+    price = scrapy.Field()
+    rating = scrapy.Field()
+    guests = scrapy.Field()
+    bedrooms = scrapy.Field()
+    beds = scrapy.Field()
+    baths = scrapy.Field()
+    minimum_stay = scrapy.Field()  # defaults to 1
+    current_price_per_night = scrapy.Field()
+
+    # Lists
+    months_ahead_unavailable_dates = scrapy.Field()  # 6 months ahead
+    months_ahead_price_per_night = scrapy.Field()  # 6 months ahead
+
+    # Strings
+    destination = scrapy.Field()
+    title = scrapy.Field()
+    features = scrapy.Field()
+    link = scrapy.Field()
+    cancellation_policy = scrapy.Field()
+
+    # Bools
+    superhost = scrapy.Field()
