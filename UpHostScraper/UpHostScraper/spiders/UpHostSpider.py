f891e002778ebd91deee9d3d9064b32b195777e1
Hazem Elsayed <hazemm.els@gmail.com>
2022-10-21 21:45:14 +0300

    set up spider navigation

--- UpHostScraper/UpHostScraper/spiders/UpHostSpider.py
+++ UpHostScraper/UpHostScraper/spiders/UpHostSpider.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from xmlrpc.client import ResponseError
 from scrapy import Spider, Request
 from UpHostScraper.utils import formatted_and_day_later
 from datetime import datetime
@@ -6,6 +7,7 @@ from datetime import datetime
 
 class UpHostSpider(Spider):
     name = "UpHostSpider"
+    base_url = "https://www.airbnb.com"
 
     def __init__(self, destination_string, checkin_date: datetime = None):
         super(UpHostSpider, self).__init__()
@@ -14,11 +16,21 @@ class UpHostSpider(Spider):
 
     def start_requests(self):
         checkin, checkout = formatted_and_day_later(self.checkin_date)
-        url = f"https://www.airbnb.com/s/{self.destination_string}/homes?tab_id=all_tab&refinement_paths%5B%5D=%2Fhomes&source=structured_search_input_header&checkin={checkin}&checkout={checkout}&search_type=search_query"
+        url = f"{self.base_url}/s/{self.destination_string}/homes?checkin={checkin}&checkout={checkout}&search_type=search_query"
 
         yield Request(url=url, callback=self.parse)
 
     def parse(self, response):
+        listing_urls = response.css("a.ln2bl2p::attr(href)").getall()
+
+        for url in listing_urls:
+            yield Request(self.base_url+url, self.parse_listing_page)
+
+        next_page = response.css("a._1bfat5l::attr(href)").get()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
+
+    def parse_listing_page(self, response):
         pass
 
     def _extract_title(self, response):
